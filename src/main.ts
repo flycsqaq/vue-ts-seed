1f77d27680036c37a6a0d89860b939730b9ce648
zhoufeiyu <119775549@qq.com>
2019-03-24 14:08:51 +0800

    config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueRx from 'vue-rx';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+
+Vue.use(VueRx);
 Vue.config.productionTip = false;
 
 new Vue({
